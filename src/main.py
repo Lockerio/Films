bd90205387d4b5717971c32c74d4295fd12a252b
Lockerio <goryunov.arkadiy@gmail.com>
2023-08-07 19:55:00 +0800

    Add styles

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    films = ["sa", "wdawd"]
+    return render_template("main.html", films=films)
 
 
 if __name__ == '__main__':
